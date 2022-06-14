2290825b88a1ca812ed826e92e95378244e05449
nonchris <git@chris-ge.de>
2022-06-14 16:28:59 +0200

    Add DropdownMaker() to generate a set of DropDown Menus from selections in a json file

--- src/bot/cogs/roles.py
+++ src/bot/cogs/roles.py
@@ -58,6 +58,68 @@ class RoleDropdown(discord.ui.Select):
         await interaction.response.send_message(f"Your roles were updated", ephemeral=True)
 
 
+class DropdownMaker:
+    def __init__(self, guild: discord.Guild, member: discord.Member,
+                 roles_menu="character",
+                 path_to_roles_json="data/roles.json"):
+        self.guild = guild
+        self.member = member
+        self.name = roles_menu
+        self.roles_json: dict[str, list[int]] = {}
+
+        self.role_ids: list[int] = self.read_json(path_to_roles_json, guild, key=roles_menu)
+
+        self.roles: list[discord.Role] = self.convert_ids_to_roles(guild)
+
+    def read_json(self, file: str, guild: discord.Guild, key: str) -> list[int]:
+        with open(file, "r") as f:
+            self.roles_json = json.load(f)
+
+        self.role_ids = self.roles_json[str(guild.id)]["roles"][key]
+        return self.role_ids
+
+    def convert_ids_to_roles(self, guild: discord.Guild) -> list[discord.Role]:
+        self.roles = [guild.get_role(r_id) for r_id in self.role_ids]
+        return self.roles
+
+    def get_role_menus(self, max_len=25, min_values=0, max_values=None) -> list[RoleDropdown]:
+        # if no limit is set, assume that all options can be chosen
+        if max_values is None:
+            max_values = max_len
+
+        # holds list in which the whole list of options is split into
+        # these lists will be used to generate the drop down menus
+        divided_options_list = []
+
+        # holds the options that will be wrapped inside a single DropDown menu
+        menu_items = []
+        for i, role in enumerate(self.roles):
+
+            # add role to menu_items
+            menu_items.append(role)
+
+            # menu is full, add it to list or this was the last element, so we need to add this un-full option menu
+            if len(menu_items) == max_len or i == len(self.roles) - 1:
+                divided_options_list.append(menu_items)
+
+                menu_items = []  # reset list, elements from that list are now options
+
+        # only enumerate when more than one menu is generated
+        is_enumeration = True if len(divided_options_list) > 1 else False
+        # holds all dropdown-menu items created
+        dropdown_list = []
+        for i, divided_option in enumerate(divided_options_list):
+            dropdown_list.append(RoleDropdown(self.guild, self.member, divided_option,
+                                              name=self.name,
+                                              is_enumeration=is_enumeration,
+                                              number=i + 1,  # start counting at one
+                                              min_values=min_values,
+                                              # max options is either the allowed max or all options in dropdown
+                                              max_values=min(max_values, len(divided_option))))
+
+        return dropdown_list
+
+
 class DropdownView(discord.ui.View):
     """ UI helper that warps the options"""
 
