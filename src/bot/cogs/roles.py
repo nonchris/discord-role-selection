300331a4eca045dc490bcab28961042a8a1111aa
nonchris <git@chris-ge.de>
2022-06-14 16:25:45 +0200

    Change RoleDropdown class to take a list of roles and create a DrowDown from it

--- src/bot/cogs/roles.py
+++ src/bot/cogs/roles.py
@@ -1,44 +1,37 @@
 import json
-from typing import Iterable
+from typing import Literal
 
 import discord
+from discord import app_commands
 from discord.ext import commands
+
+from ..environment import ROLES_JSON
 
 
 class RoleDropdown(discord.ui.Select):
     def __init__(self, guild: discord.Guild, member: discord.Member,
-                 roles_menu="character",
-                 path_to_roles_json="data/roles.json"):
-        self.roles_json: dict[str, list[int]] = {}
+                 roles_to_choose: list[discord.Role], name="", is_enumeration=True, number=0, min_values=0,
+                 max_values=25):
 
-        self.role_ids: list[int] = self.read_json(path_to_roles_json, key=roles_menu)
+        self.roles: list[discord.Role] = roles_to_choose
 
-        self.roles: list[discord.Role] = self.convert_ids_to_roles(guild)
-
+        # options specific for member
         self.sel_options = self.make_options(member)
 
-        super().__init__(placeholder=f"Choose the {roles_menu} you want :)",
-                         min_values=0,
-                         max_values=len(self.sel_options) - 1,
-                         options=self.sel_options),
+        super().__init__(placeholder=f"{f'#{number}' if is_enumeration > 1 else ''} Choose the {name} you want :)",
+                         min_values=min_values,
+                         max_values=max_values,
+                         options=self.sel_options)
 
-    def read_json(self, file: str, key: str) -> list[int]:
-        with open(file, "r") as f:
-            self.roles_json = json.load(f)
-
-        self.role_ids = self.roles_json["roles"][key]
-        return self.role_ids
-
-    def convert_ids_to_roles(self, guild: discord.Guild) -> list[discord.Role]:
-        self.roles = [guild.get_role(r_id) for r_id in self.role_ids]
-        return self.roles
-
-    def make_options(self, member: discord.Member):
+    def make_options(self, member: discord.Member) -> list[discord.SelectOption]:
+        """ Make options specific for member from select options """
+        # wrap each role inside an SelectOption
         self.sel_options = []
-        print(self.roles)
         for role in self.roles:
-            print(role)
-            option = discord.SelectOption(label=str(role.id), description=f"{role.name}", emoji=role.unicode_emoji)
+            option = discord.SelectOption(label=f"{role.name}", value=str(role.id),
+                                          description=f"See the #{role.name} channel", emoji=role.unicode_emoji)
+
+            # see if role shall be selected because user has this role already
             if role in member.roles:
                 option.default = True
 
@@ -46,9 +39,9 @@ class RoleDropdown(discord.ui.Select):
 
         return self.sel_options
 
-    async def callback(self, interaction: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction, reason="User chosen using dropdown menu"):
         guild = interaction.guild
-        member = interaction.message.author
+        member: discord.Member = interaction.user
 
         # all roles from that menu the user has at the moment (roles not given trough that menu removed via intersect)
         member_roles_set = set(member.roles).intersection(self.roles)
@@ -56,11 +49,11 @@ class RoleDropdown(discord.ui.Select):
 
         # roles member selected but does not have yet
         to_give = selected_roles_set.difference(member_roles_set)
-        await member.add_roles(*to_give)
+        await member.add_roles(*to_give, reason=reason)
 
         # roles member has but does not want
         to_remove = member_roles_set.difference(selected_roles_set)
-        await member.remove_roles(*to_remove)
+        await member.remove_roles(*to_remove, reason=reason)
 
         await interaction.response.send_message(f"Your roles were updated", ephemeral=True)
 
