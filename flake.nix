ecbb5b29df538da40eec9020e8f10e8fc94b9247
MayNiklas <info@niklas-steffen.de>
2022-08-21 16:25:44 +0200

    use stable discord.py @ 2.0.0 release

--- flake.nix
+++ flake.nix
@@ -32,24 +32,8 @@
               src = self;
               doCheck = false;
               propagatedBuildInputs = [
-                (discordpy.overrideAttrs
-                  (old: rec {
-                    pname = "discord.py";
-                    version = "2.0.0a4370+g868476c9";
-                    src = fetchFromGitHub {
-                      owner = "Rapptz";
-                      repo = pname;
-                      rev = "903e2e64e9182b8d3330ef565af7bb46ff9f04da";
-                      sha256 = "sha256-u17sG3mjJf19euZ0CHvJwnvhb16F3WyAQt/w+RZFu1Y=";
-                    };
-                  }))
+                discordpy
               ];
-
-              # removes install_requires from the setup.py
-              # version numbers of discord.py are still broken
-              preBuild = ''
-                sed -i '32d' setup.py
-              '';
 
               meta = with lib; {
                 description =
